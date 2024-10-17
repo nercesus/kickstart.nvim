80a499da87a4490fa9793c33ee48125338f245d8
nercesus <146875177+nercesus@users.noreply.github.com>
2024-10-17 07:17:33 -0700

    Fix startup not working outside of nvim config directory

--- lua/custom/plugins/startup.lua
+++ lua/custom/plugins/startup.lua
@@ -2,6 +2,6 @@ return {
   'startup-nvim/startup.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
   config = function()
-    require('startup').setup(require 'config.startup_frieren')
+    require('startup').setup(require 'custom.plugins.startup.frieren')
   end,
 }
